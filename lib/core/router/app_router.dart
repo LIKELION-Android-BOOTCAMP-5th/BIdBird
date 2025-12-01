29afceebe11fd65314e24280bd9668f93cf312c8
kmj685 <kmj202005@naver.com>
2025-12-01 18:11:25 +0900

    goRouter 수정

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,7 +1,12 @@
+import 'package:bidbird/core/widgets/bottom_nav_bar.dart';
+import 'package:bidbird/core/widgets/splash_screen.dart';
 import 'package:bidbird/features/auth/ui/auth_ui.dart';
 import 'package:bidbird/features/auth/viewmodel/auth_view_model.dart';
+import 'package:bidbird/features/bid/ui/bid_screen.dart';
+import 'package:bidbird/features/chat/ui/chat_screen.dart';
 import 'package:bidbird/features/feed/ui/home_screen.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:bidbird/features/profile/ui/profile_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
@@ -17,6 +22,43 @@ GoRouter createAppRouter(BuildContext context) {
         builder: (context, state) {
           return const LoginScreen();
         },
+      ),
+      ShellRoute(
+        builder: (context, state, child) {
+          return Scaffold(body: child, bottomNavigationBar: BottomNavBar());
+        },
+        routes: [
+          GoRoute(
+            path: '/splash',
+            pageBuilder: (context, state) {
+              return const NoTransitionPage(child: SplashScreen());
+            },
+          ),
+          GoRoute(
+            path: '/home',
+            pageBuilder: (context, state) {
+              return const NoTransitionPage(child: HomeScreen());
+            },
+          ),
+          GoRoute(
+            path: '/bid',
+            pageBuilder: (context, state) {
+              return const NoTransitionPage(child: BidScreen());
+            },
+          ),
+          GoRoute(
+            path: '/chat',
+            pageBuilder: (context, state) {
+              return const NoTransitionPage(child: ChatScreen());
+            },
+          ),
+          GoRoute(
+            path: '/profile',
+            pageBuilder: (context, state) {
+              return const NoTransitionPage(child: ProfileScreen());
+            },
+          ),
+        ],
       ),
       GoRoute(
         path: '/home',
