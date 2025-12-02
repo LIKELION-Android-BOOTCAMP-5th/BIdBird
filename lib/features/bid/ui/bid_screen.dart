bf40b7d9cd2fa9272f38fd31e743535767dddf82
nohyeongteak <yt873588@gmail.com>
2025-12-02 14:15:42 +0900

    구글로그인 ios/aos 완료 로그아웃 기능 버튼 프로필 페이지에 임시로 저장 완료 팝업 컴포넌트 이식 완료(ui 손 봐야함)

--- lib/features/bid/ui/bid_screen.dart
+++ lib/features/bid/ui/bid_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/utils/ui_set/icons.dart';
+
 class BidScreen extends StatelessWidget {
   const BidScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('입찰내역')));
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('입찰내역'),
+            Image.asset(
+              'assets/icons/alarm_icon.png',
+              width: iconSize.width,
+              height: iconSize.height,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
