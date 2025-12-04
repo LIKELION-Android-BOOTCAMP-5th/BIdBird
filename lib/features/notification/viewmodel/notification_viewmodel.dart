73032f6187169687a3c103ae10bb9e8b2d1417d8
kmj685 <kmj202005@naver.com>
2025-12-04 15:16:15 +0900

    폴더구조 정리(notification 폴더 참고 바람)

---
+++ lib/features/notification/viewmodel/notification_viewmodel.dart
@@ -0,0 +1,41 @@
+// 뷰모델 역할
+// 화면의 상테 저장(데이터 변경시 화면을 다시 그리라고 notifyListeners()를 사용
+
+//
+
+//이것은 복붙용입니다. 혹시 몰라서 아직은 삭제하지 않으나 곧 삭제할 예정입니다.
+import 'package:flutter/material.dart';
+
+class EmptyViewModel extends ChangeNotifier {
+  // int inputCount = 0;
+
+  // final TextEditingController textEditingController = TextEditingController();
+
+  // 뷰모델 생성자, context를 통해 리포지토리를 받아올 수 있음.
+  EmptyViewModel(BuildContext context) {}
+
+  // 입력한 글자 수를 받아오는 함수
+  // void handleTextInput(String input) {
+  //   inputCount = input.length;
+  //   notifyListeners();
+  // }
+
+  // Future 함수는 API나 Supabase, 메소드 채널과 사용할 때,
+  // 처리를 하는데 시간이 걸리는 작업을 할때 사용됨.
+  // 아래 함수 안 에서는 시간이 필요한 작업은 await를 사용 해야함
+  // Future<void> function() async {
+  //   try {
+  //
+  //   } catch (e) {
+  //
+  //   }
+  //
+  //   // 리빌딩, 리콤포지션 진행
+  //   notifyListeners();
+  // }
+  // Future<void> function1() async {
+  //   // 시간 지연 코드
+  //   await Future.delayed(const Duration(milliseconds: 1700));
+  //   notifyListeners();
+  // }
+}
