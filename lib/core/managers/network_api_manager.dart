dabe7b78b6e45872d86de990a1058377948413d1
nohyeongteak <yt873588@gmail.com>
2025-12-08 09:10:40 +0900

    UPDATE/ 홈 화면 모든 api 데이터 fetch 완료, 홈화면 페이징 처리 완료

--- lib/core/managers/network_api_manager.dart
+++ lib/core/managers/network_api_manager.dart
@@ -1,3 +1,4 @@
+import 'package:bidbird/core/models/items_entity.dart';
 import 'package:dio/dio.dart';
 
 import '../../features/feed/model/home_data.dart';
@@ -27,6 +28,42 @@ class NetworkApiManager {
     print("data 데이터 타입: ${data.runtimeType}");
     final List<HomeCodeKeywordType> results = data.map((json) {
       return HomeCodeKeywordType.fromJson(json);
+    }).toList();
+
+    return results;
+  }
+
+  Future<List<ItemsEntity>> getItems({
+    int currentIndex = 1,
+    int perPage = 8,
+  }) async {
+    int startIndex = currentIndex - 1;
+    int endIndex = perPage - 1;
+
+    // 현재 페이지가 첫 페이지가 아니라면
+    if (currentIndex != 1) {
+      endIndex = (currentIndex * perPage) - 1;
+      startIndex = (currentIndex - 1) * perPage;
+    }
+
+    final String range = "${startIndex}-${endIndex}";
+
+    final response = await dio.get(
+      'https://mdwelwjletorehxsptqa.supabase.co/rest/v1/items?select=*',
+      options: Options(
+        headers: {
+          'apikey':
+              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kd2Vsd2psZXRvcmVoeHNwdHFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyOTEwNzksImV4cCI6MjA3OTg2NzA3OX0.tpCDNi74KoMcpr3BN7D6fT2SxsteCM9sf7RrEwnVPHg',
+          'Authorization':
+              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kd2Vsd2psZXRvcmVoeHNwdHFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyOTEwNzksImV4cCI6MjA3OTg2NzA3OX0.tpCDNi74KoMcpr3BN7D6fT2SxsteCM9sf7RrEwnVPHg',
+          'Range': range,
+        },
+      ),
+    );
+    final List<dynamic> data = response.data;
+    print("data 데이터 타입: ${data.runtimeType}");
+    final List<ItemsEntity> results = data.map((json) {
+      return ItemsEntity.fromJson(json);
     }).toList();
 
     return results;
--- lib/features/feed/viewmodel/home_viewmodel.dart
+++ lib/features/feed/viewmodel/home_viewmodel.dart
@@ -1,20 +1,92 @@
+import 'dart:async';
+
+import 'package:bidbird/core/models/items_entity.dart';
 import 'package:flutter/widgets.dart';
 
 import '../model/home_data.dart';
 import '../repository/home_repository.dart';
+
+//최신순, 오래된순, 좋아요순 처리할 때 쓸 것
+enum OrderByType { newFirst, oldFirst, likesFirst }
 
 class HomeViewmodel extends ChangeNotifier {
   final HomeRepository _homeRepository;
   //키워드 그릇 생성
   List<HomeCodeKeywordType> _keywords = [];
   List<HomeCodeKeywordType> get keywords => _keywords;
+  //Items 그릇 생성
+  List<ItemsEntity> _Items = [];
+  List<ItemsEntity> get Items => _Items;
+  bool buttonIsWorking = false;
+  OrderByType type = OrderByType.newFirst;
+
+  //페이징 처리
+  int _currentPage = 1;
+  int get currentPage => _currentPage;
+  //스크롤 컨트롤러
+  ScrollController scrollController = ScrollController();
 
   HomeViewmodel(this._homeRepository) {
     getKeywordList();
+    Timer? _debounce;
+
+    // 스크롤 fetch 설정 부분, 여기서 기본적인 fetch도 이루어짐
+    scrollController.addListener(() async {
+      if (_debounce?.isActive ?? false) _debounce!.cancel();
+
+      _debounce = Timer(const Duration(milliseconds: 300), () {
+        final double offset = scrollController.offset;
+
+        if (offset < 50) {
+          if (buttonIsWorking) {
+            buttonIsWorking = false;
+            notifyListeners();
+          }
+        } else {
+          if (!buttonIsWorking) {
+            buttonIsWorking = true;
+            notifyListeners();
+          }
+        }
+      });
+
+      if (scrollController.position.atEdge &&
+          scrollController.position.pixels != 0) {
+        fetchNextItems();
+      }
+      //이걸 추가해야지 더욱 안정적이로 스크롤 닿기 전에 이미 fetch되어서 자연스러움
+      if (scrollController.position.pixels >=
+          scrollController.position.maxScrollExtent - 200) {
+        fetchNextItems();
+      }
+    });
+    fetchItems();
   }
 
   Future<void> getKeywordList() async {
     keywords.addAll(await _homeRepository.getKeywordType());
+    notifyListeners();
+  }
+
+  Future<void> fetchItems({int currentIndex = 1}) async {
+    _Items = await _homeRepository.fetchItems(currentIndex: _currentPage);
+    notifyListeners();
+  }
+
+  Future<void> handleRefresh() async {
+    _currentPage = 1;
+    _Items = [];
+    notifyListeners();
+    _Items = await _homeRepository.fetchItems(currentIndex: _currentPage);
+    notifyListeners();
+  }
+
+  Future<void> fetchNextItems() async {
+    _currentPage++;
+    List<ItemsEntity> newFetchPosts = await _homeRepository.fetchItems(
+      currentIndex: _currentPage,
+    );
+    _Items.addAll(newFetchPosts);
     notifyListeners();
   }
 }
