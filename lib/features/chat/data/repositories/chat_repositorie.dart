0cdd3e6aa34cc8020fe6d54efd3e8df034bac280
kmj685 <kmj202005@naver.com>
2025-12-10 09:20:24 +0900

    채팅 화면 화면(realtime 미구현),채팅방 화면UI(30%), 채팅 기능 구현중

--- lib/features/chat/data/repositories/chat_repositorie.dart
+++ lib/features/chat/data/repositories/chat_repositorie.dart
@@ -2,6 +2,7 @@ import 'package:bidbird/features/chat/data/datasources/network_api_chat_datasour
 import 'package:bidbird/features/chat/data/datasources/supabase_chat_datasource.dart';
 import 'package:bidbird/features/chat/model/chat_message_entity.dart';
 import 'package:bidbird/features/chat/model/chatting_room_entity.dart';
+import 'package:bidbird/features/chat/viewmodel/chatting_room_viewmodel.dart';
 
 class ChatRepositorie {
   final NetworkApiChatDatasource _networkApiChatDatasource =
@@ -18,5 +19,35 @@ class ChatRepositorie {
 
   Future<String?> getRoomId(String itemId) async {
     return await _chatDatasource.getRoomId(itemId);
+  }
+
+  Future<void> sendImageMessage(String roomId, String imageUrl) async {
+    await _chatDatasource.sendImageMessage(roomId, imageUrl);
+  }
+
+  Future<void> sendTextMessage(String roomId, String message) async {
+    await _chatDatasource.sendTextMessage(roomId, message);
+  }
+
+  Future<String?> firstMessage({
+    required String itemId,
+    String? message,
+    required MessageType messageType,
+    String? imageUrl,
+  }) async {
+    switch (messageType) {
+      case MessageType.text:
+        return await _networkApiChatDatasource.firstMessage(
+          itemId: itemId,
+          message: message,
+          message_type: "text",
+        );
+      case MessageType.image:
+        return await _networkApiChatDatasource.firstMessage(
+          itemId: itemId,
+          message_type: "image",
+          imageUrl: imageUrl,
+        );
+    }
   }
 }
