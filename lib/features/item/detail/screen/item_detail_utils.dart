178b4fbe2cd7dde7aeaf10b8bf76a8986219d636
kimjaehyun <kimdzhekhon@github.com>
2025-12-12 17:05:00 +0900

    동영상 썸네일 오류 해결

--- lib/features/item/detail/screen/item_detail_utils.dart
+++ lib/features/item/detail/screen/item_detail_utils.dart
@@ -10,13 +10,10 @@ String formatRemainingTime(DateTime finishTime) {
 }
 
 bool isVideoFile(String filePathOrUrl) {
-  // URL이나 파일 경로에서 확장자 추출
   String path = filePathOrUrl;
-  // URL인 경우 쿼리 파라미터 제거
   if (path.contains('?')) {
     path = path.split('?').first;
   }
-  // URL인 경우 경로만 추출
   if (path.contains('/')) {
     path = path.split('/').last;
   }
@@ -25,32 +22,23 @@ bool isVideoFile(String filePathOrUrl) {
   return videoExtensions.contains(extension);
 }
 
-/// Cloudinary 동영상 URL에서 썸네일 이미지 URL 생성
 String getVideoThumbnailUrl(String videoUrl) {
-  // Cloudinary 동영상 URL에서 썸네일 생성
-  // 예: https://res.cloudinary.com/.../video/upload/v123/abc.mp4
-  // -> https://res.cloudinary.com/.../video/upload/so_0/v123/abc.jpg
   if (videoUrl.contains('/video/upload/')) {
     try {
-      // URL을 파싱하여 썸네일 URL 생성
       final uri = Uri.parse(videoUrl);
       final pathSegments = uri.pathSegments;
       
-      // video/upload 다음에 변환 파라미터와 버전, 파일명이 있음
       final videoIndex = pathSegments.indexOf('video');
       if (videoIndex >= 0 && videoIndex < pathSegments.length - 1) {
         if (pathSegments[videoIndex + 1] == 'upload') {
-          // video/upload/ 이후의 경로를 가져옴
           final afterUpload = pathSegments.sublist(videoIndex + 2);
           if (afterUpload.isNotEmpty) {
-            // 마지막 세그먼트가 파일명 (확장자 포함)
             final fileName = afterUpload.last;
             final fileNameWithoutExt = fileName.split('.').first;
             
-            // so_0은 동영상의 첫 프레임을 의미하는 변환 파라미터
             final thumbnailPath = [
               ...pathSegments.sublist(0, videoIndex + 2),
-              'so_0', // 동영상의 첫 프레임
+              'so_0',
               ...afterUpload.sublist(0, afterUpload.length - 1),
               '$fileNameWithoutExt.jpg',
             ];
@@ -60,10 +48,8 @@ String getVideoThumbnailUrl(String videoUrl) {
         }
       }
     } catch (e) {
-      // 파싱 실패 시 원본 URL 반환
     }
   }
-  // Cloudinary URL이 아니거나 파싱 실패 시 원본 URL 반환
   return videoUrl;
 }
 
