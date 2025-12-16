d12f6e476cf91ac748c0b31ed34edea8efc5efe1
nohyeongteak <yt873588@gmail.com>
2025-12-16 18:10:00 +0900

    UPDATE / 약관 동의 <-> 로그인 화면 무한 루프 현상 해결 약관 동의 화면 뒤로가기 버튼 없앰

--- lib/features/auth/ui/tos_screen.dart
+++ lib/features/auth/ui/tos_screen.dart
@@ -11,56 +11,60 @@ class ToSScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Scaffold(
-      backgroundColor: theme.scaffoldBackgroundColor,
-      appBar: AppBar(
-        title: const Text('경매 서비스 이용 약관'),
-        backgroundColor: theme.appBarTheme.backgroundColor,
-      ),
-      body: SafeArea(
-        child: Consumer<ToSViewmodel>(
-          builder: (context, viewmodel, child) {
-            if (viewmodel.tosInfo.isEmpty) {
-              return const Center(child: CircularProgressIndicator());
-            }
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        backgroundColor: theme.scaffoldBackgroundColor,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('경매 서비스 이용 약관'),
+          backgroundColor: theme.appBarTheme.backgroundColor,
+        ),
+        body: SafeArea(
+          child: Consumer<ToSViewmodel>(
+            builder: (context, viewmodel, child) {
+              if (viewmodel.tosInfo.isEmpty) {
+                return const Center(child: CircularProgressIndicator());
+              }
 
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Text(
-                        viewmodel.tosInfo.first.content,
-                        style: const TextStyle(fontSize: 14),
-                      ),
-                    ),
-                  ),
-
-                  const SizedBox(height: 16),
-
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        viewmodel.tosAgreed();
-                        context.go('/login/ToS/auth_set_profile');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: blueColor,
-                        foregroundColor: BackgroundColor,
-                        padding: const EdgeInsets.symmetric(vertical: 12),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: defaultBorder,
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Text(
+                          viewmodel.tosInfo.first.content,
+                          style: const TextStyle(fontSize: 14),
                         ),
                       ),
-                      child: const Text('확인했습니다'),
                     ),
-                  ),
-                ],
-              ),
-            );
-          },
+
+                    const SizedBox(height: 16),
+
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          viewmodel.tosAgreed();
+                          context.go('/login/ToS/auth_set_profile');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: blueColor,
+                          foregroundColor: BackgroundColor,
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: defaultBorder,
+                          ),
+                        ),
+                        child: const Text('확인했습니다'),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
